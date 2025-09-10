9b946203d405b8dbf900d66a3b3185f00499c211
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2025-09-10 08:13:12 -0400

    Address @wilsonsj100 feedback

--- run-current-data.R
+++ run-current-data.R
@@ -6,13 +6,8 @@
 
 if(Sys.getenv("CI") == "") {
   # Normal usage
-  DROPBOX <- "~/Dropbox"
-  if(file.exists(DROPBOX)) {
-    message("Found Dropbox")
-  } else {
-    message("Please select a file in Dropbox")
+    message("Please select any file in Dropbox top level")
     DROPBOX <- dirname(file.choose())
-  }
 } else {
   # Running on GitHub Actions
   message("Running on GitHub Actions!")
