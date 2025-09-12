dca645debb1bcc84e36a24d2dca6e1e050e325ac
robertapeixoto <roberta.bittencourtpeixoto@utoledo.edu>
2025-09-12 11:12:39 -0400

    Adapting run-current-for-windows

---
+++ run-current-windows-data.R
@@ -0,0 +1,21 @@
+# run-current-data.R
+# Ask the user where the Dropbox folder is and run the qmd file
+# If on GitHub Actions, use the testing folder
+# BBL September 2025
+
+#adapting for windows
+
+if(Sys.getenv("CI") == "") {
+  # Normal usage
+    message("Please select any file in Dropbox top level")
+    DROPBOX <- dirname(file.choose())
+} else {
+  # Running on GitHub Actions
+  message("Running on GitHub Actions!")
+  DROPBOX <- "./testing/"
+}
+
+library(quarto)
+quarto_render("current-data.qmd", execute_params = list(DROPBOX = DROPBOX))
+
+message("All done")
