4ea520ca2a9a10a1a683bff2fa84292e93532674
zoeread <51928532+zoeread@users.noreply.github.com>
2026-03-30 16:51:57 -0400

    Update run-current-data.R

--- run-current-data.R
+++ run-current-data.R
@@ -17,7 +17,7 @@ if(Sys.getenv("CI") == "") {
   SITES <- "TESTING"
 }
 
-quarto_render("current-data-zr.qmd", 
+quarto_render("current-data.qmd", 
               execute_params = list(DROPBOX = DROPBOX, SITES = SITES))
                                                         
 message("All done")
